f17ad1ac9a936b26a52dde494cf52e7f2b7b76a4
Akanksha <ag6389936@gmail.com>
2023-02-10 03:16:49 +0530

    initial commit

---
+++ business-card1_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,69 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void openURL::Start()
+extern void openURL_Start_mA032618FD2DD370EE1B4CA4F0B92C9DEB33763E6 (void);
+// 0x00000002 System.Void openURL::Update()
+extern void openURL_Update_mA9B9E8D65F6C0A535F67EBCB8B8FA5FC1BA6D848 (void);
+// 0x00000003 System.Void openURL::yt()
+extern void openURL_yt_m0B58A33572800A6288F69D52C10BF3C486C142AC (void);
+// 0x00000004 System.Void openURL::instagram()
+extern void openURL_instagram_m31F1863FFBDB0CF868369F23053D641E6635C4DE (void);
+// 0x00000005 System.Void openURL::Linkedin()
+extern void openURL_Linkedin_mDFD8A53F3EA66E4E8F5619CD3BC1DBC431FE4DFC (void);
+// 0x00000006 System.Void openURL::Google()
+extern void openURL_Google_mF32307B240F3C10374EDEA742783A83E4412EC03 (void);
+// 0x00000007 System.Void openURL::.ctor()
+extern void openURL__ctor_m0D923CE2B2F8D962C0E2FF2A9F81048BD7CE25CD (void);
+static Il2CppMethodPointer s_methodPointers[7] = 
+{
+	openURL_Start_mA032618FD2DD370EE1B4CA4F0B92C9DEB33763E6,
+	openURL_Update_mA9B9E8D65F6C0A535F67EBCB8B8FA5FC1BA6D848,
+	openURL_yt_m0B58A33572800A6288F69D52C10BF3C486C142AC,
+	openURL_instagram_m31F1863FFBDB0CF868369F23053D641E6635C4DE,
+	openURL_Linkedin_mDFD8A53F3EA66E4E8F5619CD3BC1DBC431FE4DFC,
+	openURL_Google_mF32307B240F3C10374EDEA742783A83E4412EC03,
+	openURL__ctor_m0D923CE2B2F8D962C0E2FF2A9F81048BD7CE25CD,
+};
+static const int32_t s_InvokerIndices[7] = 
+{
+	5609,
+	5609,
+	5609,
+	5609,
+	5609,
+	5609,
+	5609,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	7,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
