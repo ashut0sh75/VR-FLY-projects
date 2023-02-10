4d4c145c47f68982ab61de7d482ec8313b870c1c
Ashutosh Gupta <ashusanugupta999@gmail.com>
2023-02-10 10:26:16 +0530

    Ashutosh

---
+++ Business_card/Assets/scripts/LinkManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinkManager : MonoBehaviour
+{
+ public void openLinkedin()
+ {
+    Application.OpenURL("https://www.linkedin.com/in/ashutosh-gupta-708a65232");
+ }
+ 
+ public void openInstagram()
+ {
+    Application.OpenURL("https://instagram.com/ashutosh_gupta75?igshid=YzgyMTM2MGM=");
+ }
+
+public void openGithub()
+{
+    Application.OpenURL("https://github.com/ashut0sh75");
+}
+
+}
